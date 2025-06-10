3b592c643f499ca9a5db5ef6901407ff0f84f137
ecnu_pc <821278796@qq.com>
2025-06-10 19:17:39 +0800

    update project structure

--- predict.py
+++ predict.py
@@ -1,19 +1,13 @@
 import os.path
 from models.model_util.GeoAttX import GeoAttX_I, GeoAttX_P
 import numpy as np
+from configs.predict_config import predict_minutes, file_path, file_name, x1_path, x4_path, x12_path, model_path
 
 if __name__ == '__main__':
-    x1_path = 'models/GeoAttX_I_x1.pt'
-    x4_path = 'models/GeoAttX_I_x4.pt'
-    x12_path = 'models/GeoAttX_I_x12.pt'
-    predict_minutes = 180
-    file_path = r'data_dir'
-    file_name = r'FY4B-_AGRI--_N_DISK_1050E_L1-_FDI-_MULT_NOM_20241201001500_20241201002959_4000M_V0001.HDF'
     I_net = GeoAttX_I(file_path, x1_path, x4_path, x12_path, config='./configs/config_predict.yml')
     ys = I_net.predict(file_name, predict_minutes, p_steps=(4, 1))
     I_net.save(file_name, ys)
     # *******************************************************************************************************
-    model_path = 'models/GeoAttX_P.pt'
     net = GeoAttX_P(model_path, config='./configs/config_qpe.yml')
     for y_date, y_np in ys.items():
         input_data_path = os.path.join(net.get_root_path(), 'input.npy')
