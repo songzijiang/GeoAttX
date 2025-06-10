3eb189bbcffac824a59928167ee7f4cc8d45b9c8
ecnu_pc <821278796@qq.com>
2025-06-10 19:38:44 +0800

    update project structure

---
+++ configs/predict_config.py
@@ -0,0 +1,8 @@
+predict_minutes = 180
+file_path = r'data_dir'
+file_name = r'FY4B-_AGRI--_N_DISK_1050E_L1-_FDI-_MULT_NOM_20241201001500_20241201002959_4000M_V0001.HDF'
+
+x1_path = 'models/GeoAttX_I_x1.pt'
+x4_path = 'models/GeoAttX_I_x4.pt'
+x12_path = 'models/GeoAttX_I_x12.pt'
+model_path = 'models/GeoAttX_P.pt'
