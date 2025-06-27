34035efbb2142285f1d7109ac712465ee0676fc8
6000ada <821278796@qq.com>
2025-06-27 13:23:04 +0800

    merge

--- configs/predict_config.py
+++ configs/predict_config.py
@@ -2,7 +2,7 @@ from datetime import datetime, timedelta
 
 # common setting
 predict_minutes = 180
-file_path = r'data_dir'
+file_path = r'/mnt/data1/szj/FY/downloaded_file'
 file_name = r'FY4B-_AGRI--_N_DISK_1050E_L1-_FDI-_MULT_NOM_20241201001500_20241201002959_4000M_V0001.HDF'
 
 x1_path = 'models/GeoAttX_I_x1.pt'
@@ -11,5 +11,5 @@ x12_path = 'models/GeoAttX_I_x12.pt'
 model_path = 'models/GeoAttX_P.pt'
 
 # range_predict.py setting
-start_date = datetime(year=2025, month=6, day=7, hour=0, minute=0)
-end_date = datetime(year=2025, month=6, day=8, hour=23, minute=59)
+start_date = datetime(year=2025, month=6, day=7, hour=0, minute=0)-timedelta(hours=8)
+end_date = datetime(year=2025, month=6, day=8, hour=23, minute=59)-timedelta(hours=8)
