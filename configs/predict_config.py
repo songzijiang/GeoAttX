fa1142fd8509f4df5c529db2b5ae1899c9410d54
ecnu_pc <821278796@qq.com>
2025-06-27 13:25:13 +0800

    revert commit

--- configs/predict_config.py
+++ configs/predict_config.py
@@ -2,7 +2,7 @@ from datetime import datetime, timedelta
 
 # common setting
 predict_minutes = 180
-file_path = r'/mnt/data1/szj/FY/downloaded_file'
+file_path = r'data_dir'
 file_name = r'FY4B-_AGRI--_N_DISK_1050E_L1-_FDI-_MULT_NOM_20241201001500_20241201002959_4000M_V0001.HDF'
 
 x1_path = 'models/GeoAttX_I_x1.pt'
