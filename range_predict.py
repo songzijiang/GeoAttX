6d06942d14b2fae7e5ffc9b12f630d62fc0bb08e
ecnu_pc <821278796@qq.com>
2025-12-29 19:56:46 +0800

    upgrade Huayu Structure

--- range_predict.py
+++ range_predict.py
@@ -2,6 +2,31 @@ import os.path
 from jacksung.ai.GeoAttX import GeoAttX_I, GeoAttX_P, Huayu
 import numpy as np
 from datetime import datetime, timedelta
+from jacksung.utils.multi_task import MultiTasks
+
+
+def multi_worker(current_date):
+    I_net = GeoAttX_I(norm_path, x1_path, x4_path, x12_path, config='./configs/config_predict.yml')
+    M_net = Huayu(norm_path, HY_path, config='./configs/config_imerg.yml')
+    start_str = current_date.strftime("%Y%m%d%H%M%S")
+    print(rf'预测起始时间：{current_date.strftime("%Y-%m-%d %H:%M:%S")}')
+    I_net.set_root_path()
+    M_net.set_root_path()
+    ys = I_net.predict(current_date, predict_minutes, p_steps=(4, 1))
+    I_net.save(current_date, ys)
+    # *******************************************************************************************************
+    for y_date, y_np in ys.items():
+        if y_date != list(ys.keys())[-1]:
+            continue
+        input_data_path = os.path.join(I_net.get_root_path(), 'input.npy')
+        os.makedirs(M_net.get_root_path(), exist_ok=True)
+        np.save(input_data_path, y_np)
+        y = M_net.predict(input_data_path)
+        os.remove(input_data_path)
+        if y is not None:
+            M_net.save(y, y_date.strftime("%Y%m%d_%H%M%S"))
+    print('*' * 60)
+
 
 if __name__ == '__main__':
     # Predict the range of dates
@@ -19,25 +44,11 @@ if __name__ == '__main__':
     end_date = datetime(year=2024, month=9, day=17, hour=23, minute=59)
 
     current_date = start_date
-    I_net = GeoAttX_I(norm_path, x1_path, x4_path, x12_path, config='./configs/config_predict.yml')
-    M_net = Huayu(norm_path, HY_path, config='./configs/config_imerg.yml')
+    mt = MultiTasks(6)
     while current_date <= end_date:
-        start_str = current_date.strftime("%Y%m%d%H%M%S")
-        print(rf'预测起始时间：{current_date.strftime("%Y-%m-%d %H:%M:%S")}')
-        I_net.set_root_path()
-        M_net.set_root_path()
-        ys = I_net.predict(current_date, predict_minutes, p_steps=(4, 1))
-        I_net.save(current_date, ys)
-        # *******************************************************************************************************
-        for y_date, y_np in ys.items():
-            if y_date != list(ys.keys())[-1]:
-                continue
-            input_data_path = os.path.join(I_net.get_root_path(), 'input.npy')
-            os.makedirs(M_net.get_root_path(), exist_ok=True)
-            np.save(input_data_path, y_np)
-            y = M_net.predict(input_data_path)
-            os.remove(input_data_path)
-            if y is not None:
-                M_net.save(y, y_date.strftime("%Y%m%d_%H%M%S"))
+        # single thread
+        # multi_worker(current_date)
+        # multi threads
+        mt.add_task(current_date, multi_worker, [current_date])
         current_date += timedelta(minutes=15)
-        print('*' * 60)
+    mt.execute_task()
