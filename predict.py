59238fb0bd0d963a2b94388bfbfdc193f6b31fe3
ecnu_pc <821278796@qq.com>
2025-12-29 16:59:21 +0800

    upgrade Huayu Name

--- predict.py
+++ predict.py
@@ -1,10 +1,20 @@
 import os.path
 from jacksung.ai.GeoAttX import GeoAttX_I, GeoAttX_P, Huayu
+from jacksung.ai.utils.fy import get_agri_file_path
 import numpy as np
-from configs.predict_config import predict_minutes, file_path, file_name, x1_path, x4_path, x12_path, GP_path, \
-    HY_path
+from datetime import datetime,timedelta
 
-if __name__ == '__main__':
+if __name__ == '__main__':  # common setting
+    predict_minutes = 15
+    file_path = r'dataset'
+    file_name = r'FY4B-_AGRI--_N_DISK_1050E_L1-_FDI-_MULT_NOM_20241201231500_20241201232959_4000M_V0001.HDF'
+
+    x1_path = 'models/GeoAttX_I_x1.pt'
+    x4_path = 'models/GeoAttX_I_x4.pt'
+    x12_path = 'models/GeoAttX_I_x12.pt'
+    GP_path = 'models/GeoAttX_P.pt'
+    HY_path = 'models/GeoAttX_M.pt'
+
     I_net = GeoAttX_I(file_path, x1_path, x4_path, x12_path, config='./configs/config_predict.yml')
     ys = I_net.predict(file_name, predict_minutes, p_steps=(4, 1))
     I_net.save(file_name, ys)
@@ -25,4 +35,3 @@ if __name__ == '__main__':
             M_net.save(y_imerg, y_date.strftime("%Y%m%d_%H%M%S"))
         if y_qpe is not None:
             P_net.save(y_qpe, y_date.strftime("%Y%m%d_%H%M%S"))
-
--- range_predict.py
+++ range_predict.py
@@ -1,37 +1,46 @@
 import os.path
-from jacksung.ai.GeoAttX import GeoAttX_I, GeoAttX_P
+from jacksung.ai.GeoAttX import GeoAttX_I, GeoAttX_P, Huayu
 import numpy as np
-from configs.predict_config import predict_minutes, file_path, x1_path, x4_path, x12_path, model_path, \
-    start_date, end_date
 from datetime import datetime, timedelta
 
 if __name__ == '__main__':
     # Predict the range of dates
+    predict_minutes = 180
+    file_path = r'dataset'
+
+    x1_path = 'models/GeoAttX_I_x1.pt'
+    x4_path = 'models/GeoAttX_I_x4.pt'
+    x12_path = 'models/GeoAttX_I_x12.pt'
+    GP_path = 'models/GeoAttX_P.pt'
+    HY_path = 'models/GeoAttX_M.pt'
+
+    # range_predict.py setting
+    start_date = datetime(year=2024, month=9, day=15, hour=0, minute=0) - timedelta(hours=8)
+    end_date = datetime(year=2024, month=9, day=21, hour=2, minute=59) - timedelta(hours=8)
+
     file_names = []
     current_date = start_date
     I_net = GeoAttX_I(file_path, x1_path, x4_path, x12_path, config='./configs/config_predict.yml')
-    net = GeoAttX_P(model_path, config='./configs/config_qpe.yml')
+    M_net = Huayu(HY_path, config='./configs/config_imerg.yml')
     while current_date <= end_date:
         start_str = current_date.strftime("%Y%m%d%H%M%S")
         end_str = (current_date + timedelta(minutes=14, seconds=59)).strftime("%Y%m%d%H%M%S")
-        file_names.append(rf'FY4B-_AGRI--_N_DISK_{105 if current_date > datetime(year=2025, month=3, day=1) else 133}' +
+        file_names.append(rf'FY4B-_AGRI--_N_DISK_{105 if current_date > datetime(year=2023, month=3, day=1) else 133}' +
                           rf'0E_L1-_FDI-_MULT_NOM_{start_str}_{end_str}_4000M_V0001.HDF')
         current_date += timedelta(minutes=15)
     for idx, file_name in enumerate(file_names):
         print(rf'{idx}/{len(file_names)}, 预测起始文件：{file_name}')
         I_net.set_root_path()
-        net.set_root_path()
         ys = I_net.predict(file_name, predict_minutes, p_steps=(4, 1))
         I_net.save(file_name, ys)
         # *******************************************************************************************************
-
         for y_date, y_np in ys.items():
-            input_data_path = os.path.join(net.get_root_path(), 'input.npy')
-            os.makedirs(net.get_root_path(), exist_ok=True)
+            input_data_path = os.path.join(I_net.get_root_path(), 'input.npy')
+            os.makedirs(M_net.get_root_path(), exist_ok=True)
             np.save(input_data_path, y_np)
-            y = net.predict(input_data_path)
+            y = M_net.predict(input_data_path)
             os.remove(input_data_path)
             if y is not None:
-                net.save(y, y_date.strftime("%Y%m%d_%H%M%S"))
+                M_net.save(y, y_date.strftime("%Y%m%d_%H%M%S"))
         current_date += timedelta(minutes=15)
         print('*' * 60)
